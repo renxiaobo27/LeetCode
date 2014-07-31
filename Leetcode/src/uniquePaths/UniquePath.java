c857e64872a0357de66356ce20f368caad4eb7ce
renxiaobo27 <renxiaobo27@gmail.com>
2014-07-31 01:57:20 -0500

    Unique Path 7_31

---
+++ Leetcode/src/uniquePaths/UniquePath.java
@@ -0,0 +1,28 @@
+package uniquePaths;
+
+public class UniquePath {
+
+	public int fun(int m,int n)
+	{
+		if(m==1 || n==1) return 1;
+		if(m==0 || n==0) return 0;
+		
+		int [][]path = new int[m][n];
+		for(int i=0;i<m;i++)
+			path[i][0] = 1;
+		for(int j=0;j<n;j++)
+			path[0][j] = 1;
+		
+		for(int i=1;i<m;i++)
+		{
+			for(int j=1;j<n;j++)
+			{
+				path[i][j] = path[i-1][j] + path[i][j-1];
+			}
+		}
+		
+		return path[m-1][n-1];
+		
+	}
+	
+}
